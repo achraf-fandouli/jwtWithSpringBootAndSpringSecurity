fda5de4d8f24d7906672c89b6b19936cd40b8e0a
achraf-fandouli <achref.fendouli@gmail.com>
2024-02-29 17:37:54 +0100

    Add entity models, including AdminEntity , UserEntity , and UserType enum

---
+++ src/main/java/com/digitalcode/jwt/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package com.digitalcode.jwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+	private String username;
+	@JsonIgnore
+	private String password;
+}
