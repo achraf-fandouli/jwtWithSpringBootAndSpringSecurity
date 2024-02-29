b80789c5935ae4e35e3142eb5a14e89b2fcaf8f7
achraf-fandouli <achref.fendouli@gmail.com>
2024-02-29 16:58:23 +0100

    Required dependencies

---
+++ src/main/java/com/digitalcode/jwt/JwtWithSpringBootAndSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.digitalcode.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtWithSpringBootAndSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtWithSpringBootAndSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalcode/jwt/JwtWithSpringBootAndSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalcode.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtWithSpringBootAndSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
